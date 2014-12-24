7d8f95d663d469dd019d5cc9131abfc2a12c6d4f
sanjaymali <sanjaymalimca@gmail.com>
2014-12-24 15:19:37 +0500

    Displayed images in GridView

--- GridViewApp/src/com/example/gridviewapp/ImageAdapter.java
+++ GridViewApp/src/com/example/gridviewapp/ImageAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 	private Context mContext;
@@ -12,9 +14,8 @@ public class ImageAdapter extends BaseAdapter {
 		mContext=context;
 	}
 	public int getCount() {
-		
-		
-		return 0;
+
+		return mThumbIds.length;
 	}
 
 	@Override
@@ -29,21 +30,36 @@ public class ImageAdapter extends BaseAdapter {
 		return 0;
 	}
 
+
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		ImageView imageView;
+		if(convertView==null)
+		{
+			imageView=new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(150,150));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(5, 5, 5, 5);
+		}else
+		{
+			imageView=(ImageView) convertView;
+
+		}
+		imageView.setImageResource(mThumbIds[position]);
 		// TODO Auto-generated method stub
-		return null;
+		return imageView;
 	}
-public Integer[]mThumbIds ={
-		R.drawable.sample_0,
-		R.drawable.sample_1,
-		R.drawable.sample_2,
-		R.drawable.sample_3,
-		R.drawable.sample_4,
-		R.drawable.sample_5,
-		R.drawable.sample_6,
-		R.drawable.sample_7
-};
-	
-	
+	public Integer[]mThumbIds ={
+			R.drawable.sample_0,
+			R.drawable.sample_1,
+			R.drawable.sample_2,
+			R.drawable.sample_3,
+			R.drawable.sample_4,
+			R.drawable.sample_5,
+			R.drawable.sample_6,
+			R.drawable.sample_7
+	};
+
+
 }
